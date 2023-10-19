6aa7e97a532c61f5f49ef27d3ad317284f5f2f1f
Oshienga <malcolmoshienga@yahoo.com>
2023-10-19 15:30:16 -0700

    prints

--- printf.c
+++ printf.c
@@ -1,18 +1,21 @@
 #include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * _printf - Entry point
  * Description: 'program'
  * @format: format
  * Return: result
-*/
+ */
 
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
+	char buffer[32], *str;
 
 	va_start(args, format);
 	while (*format)
@@ -22,14 +25,12 @@ int _printf(const char *format, ...)
 			format++;
 			if (*format == 'c')
 			{
-				char c = va_arg(args, int);
-
-				count += write(1, &c, 1);
+				buffer[0] = va_arg(args, int);
+				count += write(1, buffer, 1);
 			}
 			else if (*format == 's')
 			{
-				char *str = va_arg(args, char *);
-
+				str = va_arg(args, char*);
 				if (!str)
 					str = "(null)";
 				while (*str)
@@ -37,8 +38,14 @@ int _printf(const char *format, ...)
 			}
 			else if (*format == '%')
 				count += write(1, "%", 1);
-			else
-				count += write(1, format - 1, 2);
+			else if (*format == 'p' || *format == 'd')
+			{
+				if (*format == 'p')
+					sprintf(buffer, "%p", va_arg(args, void*));
+				else if (*format == 'd')
+					sprintf(buffer, "%d", va_arg(args, int));
+				count += write(1, buffer, strlen(buffer));
+			}
 		}
 		else
 			count += write(1, format, 1);
